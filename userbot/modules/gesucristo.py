432cb2421684cce9853575f47954460c9f2236d8
zJugur3s <58880077+zJugur3s@users.noreply.github.com>
2019-12-14 14:15:56 +0100

    Update gesucristo.py

--- userbot/modules/gesucristo.py
+++ userbot/modules/gesucristo.py
@@ -1,1 +1,6 @@
-aeaeaeaeaeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+from userbot.events import register
+
+@register(outgoing=True, Pattern="^.ciao$")
+async def ciao(e):
+  await e.edit("**Ciaoo/nMerdaaa**")
+  
