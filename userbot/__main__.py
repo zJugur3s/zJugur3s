a72fe20bb5792dc0401d46f5d45205c673136791
zJugur3s <58880077+zJugur3s@users.noreply.github.com>
2019-12-14 13:22:57 +0100

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,35 +1,35 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.b (the "License");
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
 """ Userbot start point """
 
 from importlib import import_module
-from sqlite3 import connect
-from sys import argv
+import os
 
-from userbot import BRAIN_CHECKER, LOGS, bot
+from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
+
+from userbot import LOGS, bot
 from userbot.modules import ALL_MODULES
 
-db = connect("learning-data-root.check")
-cursor = db.cursor()
-cursor.execute("""SELECT * FROM BRAIN1""")
-all_rows = cursor.fetchall()
+INVALID_PH = '\nERROR: The Phone No. entered is INVALID' \
+             '\n  Tip: Use Country Code along with No.' \
+             '\n       Recheck your Phone Number'
 
-for i in all_rows:
-    BRAIN_CHECKER.append(i[0])
-db.close()
-bot.start()
+try:
+    bot.start()
+except PhoneNumberInvalidError:
+    print(INVALID_PH)
+    exit(1)
 
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("Your Bot is alive! Test it by typing .alive on any chat."
-          " Should you need assistance, head to https://t.me/userbot_support")
-LOGS.info("Your Bot Version is 2.4.2")
+LOGS.info("UserBot Avviato Correttamente Con @MagicAngel!")
 
-if len(argv) not in (1, 3, 4):
+SEM_TEST = os.environ.get("SEMAPHORE", None)
+if SEM_TEST:
     bot.disconnect()
 else:
     bot.run_until_disconnected()
