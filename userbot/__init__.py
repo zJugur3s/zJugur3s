52db2b368eb94e80fe2b97ba29939d1232380814
zJugur3s <58880077+zJugur3s@users.noreply.github.com>
2019-12-14 13:30:37 +0100

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -42,7 +42,7 @@ if version_info[0] < 3 or version_info[1] < 6:
     quit(1)
 
 # Check if the config was edited by using the already used variable
-CONFIG_CHECK = os.environ.get("___________PLOX_______REMOVE_____THIS_____LINE__________", None)
+CONFIG_CHECK = os.environ.get("", None)
 
 if CONFIG_CHECK:
     LOGS.error("Please remove the line mentioned in the first hashtag from the config.env file")
